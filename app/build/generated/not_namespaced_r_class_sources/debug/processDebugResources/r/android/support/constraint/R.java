c8d968328063f02a6b8f5df902d8ca79387810c2
João Guilherme <joaogfp07@gmail.com>
2019-06-04 21:54:02 -0300

    Projeto - criação cadastro - 04/06/19

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f0a0025;
-        public static final int end = 0x7f0a0056;
-        public static final int gone = 0x7f0a0064;
-        public static final int invisible = 0x7f0a0071;
-        public static final int left = 0x7f0a0076;
-        public static final int packed = 0x7f0a0093;
-        public static final int parent = 0x7f0a0095;
-        public static final int percent = 0x7f0a0098;
-        public static final int right = 0x7f0a009d;
-        public static final int spread = 0x7f0a00c0;
-        public static final int spread_inside = 0x7f0a00c1;
-        public static final int start = 0x7f0a00c6;
-        public static final int top = 0x7f0a00dc;
-        public static final int wrap = 0x7f0a00ed;
+        public static final int bottom = 0x7f0a0026;
+        public static final int end = 0x7f0a0050;
+        public static final int gone = 0x7f0a005e;
+        public static final int invisible = 0x7f0a006c;
+        public static final int left = 0x7f0a0071;
+        public static final int packed = 0x7f0a008e;
+        public static final int parent = 0x7f0a0090;
+        public static final int percent = 0x7f0a0093;
+        public static final int right = 0x7f0a0098;
+        public static final int spread = 0x7f0a00bb;
+        public static final int spread_inside = 0x7f0a00bc;
+        public static final int start = 0x7f0a00c1;
+        public static final int top = 0x7f0a00d7;
+        public static final int wrap = 0x7f0a00e9;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -65,12 +65,12 @@ public final class R {
         public static final int adjust_height = 0x7f0a001b;
         public static final int adjust_width = 0x7f0a001c;
         public static final int auto = 0x7f0a0021;
-        public static final int dark = 0x7f0a004a;
-        public static final int icon_only = 0x7f0a006b;
-        public static final int light = 0x7f0a0077;
-        public static final int none = 0x7f0a008d;
-        public static final int standard = 0x7f0a00c5;
-        public static final int wide = 0x7f0a00eb;
+        public static final int dark = 0x7f0a0044;
+        public static final int icon_only = 0x7f0a0065;
+        public static final int light = 0x7f0a0072;
+        public static final int none = 0x7f0a0088;
+        public static final int standard = 0x7f0a00c0;
+        public static final int wide = 0x7f0a00e7;
     }
     public static final class string {
         private string() {}
